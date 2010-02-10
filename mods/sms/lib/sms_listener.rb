015c03a6bcb1b5d75d113b5996189261f330d8c9
adammck <adam.mckaig@gmail.com>
2010-02-10 03:32:22 +0000

    added sms.css, for sms mod styles
    to style those (un)verified markers, this adds a <link> to every
    <head>, including the styles for the sms mod.

--- mods/sms/lib/sms_listener.rb
+++ mods/sms/lib/sms_listener.rb
@@ -4,6 +4,10 @@
 class SmsListener < Crabgrass::Hook::ViewListener
   include Singleton
 
+  def html_head(context)
+    stylesheet_link_tag "sms", :plugin => "sms"
+  end
+
   def user_edit_form__after_email(context)
     render :partial => "/sms/phone_number_form", :locals => context
   end
