38fe9a6df265aef8145139baeafe629fbe280dbb
adammck <adam.mckaig@gmail.com>
2010-02-16 19:44:37 +0000

    added UserExtension::Sms::InstanceMethods.send_sms
    now Users can be sent sms via Clickatell. also added a short note
    to mods/sms/README on how to get this set up.

--- mods/sms/app/models/user_extension/sms.rb
+++ mods/sms/app/models/user_extension/sms.rb
@@ -24,11 +24,40 @@ module UserExtension
         false
       end
 
+      def send_sms(text)
+        return None unless\
+          phone_number
+
+        conf = SmsMod::CLICKATELL_CONFIG
+        api = Clickatell::API.authenticate(
+          conf["api_key"], conf["username"], conf["password"])
+
+        begin
+          api.send_message(
+            phone_number, text)
+
+        # the message couldn't be sent
+        rescue Clickatell::API::Error => err
+
+          Engines.logger.warn(
+            "SMS could not be sent. Clickatell Says: %s (code: %s)" %
+            [err.message.inspect, err.code])
+
+          # Error 114: "Cannot route message"
+          # the number is probably invalid
+          if err.code == 114
+          end
+
+          return nil
+        end
+      end
+
       private
 
       def verify_new_phone_number
-        if self.phone_number_changed?
-          self.phone_number_verified = false
+        if phone_number_changed?
+          @phone_number_verified = false
+          send_sms("Test from Crabgrass")
         end
 
         true
--- mods/sms/init.rb
+++ mods/sms/init.rb
@@ -1,6 +1,14 @@
 #!/usr/bin/env ruby
 # vim: et ts=2 sts=2 sw=2
 
+require "clickatell"
+Clickatell::API.debug_mode = true
+
+module ::SmsMod
+  conf_filename = RAILS_ROOT + "/config/clickatell.yml"
+  CLICKATELL_CONFIG = YAML.load_file(conf_filename)
+end
+
 Dispatcher.to_prepare do
   apply_mixin_to_model(User, UserExtension::Sms)
   require 'sms_listener'
